a070246489f45a6a9020e4b9fe3c8f137afae263
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-01-30 18:41:44 -0300

    se crea tabla de usuarios para poder realizar ejercicios de crud

---
+++ CRUD_SQL/Usuarios.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Usuarios]
+(
+	[Id_usuario] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [rut] NCHAR(10) NULL, 
+    [nombre] NCHAR(10) NOT NULL, 
+    [apellido] NCHAR(10) NOT NULL, 
+    [edad] NCHAR(10) NOT NULL, 
+    [fecha_nacimiento] NCHAR(10) NOT NULL, 
+    [sexo] NCHAR(10) NOT NULL
+)
