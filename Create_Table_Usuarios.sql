78222845621561b2c8b37843f5b385a92963ab32
Ricardo Pavez <ricardoalfredo.pavez@gmail.com>
2018-01-31 15:29:00 -0300

    se agrega sp insert_usuario de crud_sql

---
+++ Create_Table_Usuarios.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Usuarios]
+(
+	[Id_usuario] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [rut] NVARCHAR(50) NOT NULL, 
+    [nombre] NVARCHAR(50) NULL, 
+    [apellido] NVARCHAR(50) NULL, 
+    [edad] INT NULL, 
+    [fecha_nacimiento] DATETIME NULL, 
+    [sexo] NVARCHAR(10) NULL, 
+    [ruta_de_inicio] NVARCHAR(50) NOT NULL, 
+    [ruta_de_termino] NVARCHAR(50) NOT NULL
+)
